2e0bd653c1686aba5b45c4ad79ecd2eb27d74f38
j5E01 <44983900+j5E01@users.noreply.github.com>
2018-12-14 23:51:51 +0100

    Create Program.py

---
+++ Program.py
@@ -0,0 +1,8 @@
+#this first program says hello and asks for your name
+
+print("Hey, buddy!")
+pygame.time.wait(2000)
+print("What's your name?")
+yourName = input()
+pygame.time.wait(2000)
+print("It is nice to meet you, " + yourName)
