2dce10d887a230b0a2d30b34d02a591d26c54714
j5E01 <j5E01@users.noreply.github.com>
2018-12-15 00:37:13 +0100

    install link
    added to readme

--- Program.py
+++ Program.py
@@ -1,6 +1,7 @@
 #this first program says hello and asks for your name
+import pygame
 
-print("Hey, buddy!")
+print("\n \n Hey, buddy!")
 pygame.time.wait(2000)
 print("What's your name?")
 yourName = input()
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A place to learn and play
 
 
 
-
+https://nostarch.com/download/Teach_Your_Kids_to_Code_InstallingPygame_MacLinux.pdf
 
 
 Here's some great learning Links:
