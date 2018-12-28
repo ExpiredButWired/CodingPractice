a444ccc6a9fda3e521f346ab9dfacbf310a1f403
j5E01 <j5E01@users.noreply.github.com>
2018-12-28 20:08:39 +0100

    Error Handling updated
    Made a loop to continue trying if errors are evident

--- vol_calc.py
+++ vol_calc.py
@@ -1,17 +1,27 @@
 #this is to make a volume calculator
 import pygame, sys
 from pygame.locals import *
-print('what is the length')
-length = input()
-print('what is the width')
-width = input()
-print('what is the height')
-ht = input()
-print('calculating')
-pygame.time.wait(1000)
-print(' volume equals ')
-# now to convert the strings into integers
-length = int(length)
-width = int(width)
-ht = int(ht)
-print(length * width * ht)
+
+while True:
+	try:
+		print('what is the length')
+		length = input()
+		print('what is the width')
+		width = input()
+		print('what is the height')
+		ht = input()
+		print('calculating')
+		pygame.time.wait(1000)
+		print(' volume equals ')
+		# now to convert the strings into integers
+		length = int(length)
+		width = int(width)
+		ht = int(ht)
+		answer = length * width * ht
+		if answer > 0:
+			print(answer)
+			pygame.time.wait(2000)
+			break
+	except ValueError:
+		print("\n \n \n OOPS.")
+		print("Make sure to use numbers, and not letters or special characters!\n Try again.\n \n ")
