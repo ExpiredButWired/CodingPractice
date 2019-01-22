eea875ce0836c3029504a54cd9e00bafd9973211
B_cuz <yodudewuzzup@gmail.com>
2019-01-21 22:47:16 -0500

    cruisin

--- literallyrestart.py
+++ literallyrestart.py
@@ -1,3 +1,19 @@
 import pygame, sys
 print("hello world")
-#i have cq tomorrow, so  I will move on from there. 
+print(2 + 5)
+print('your new score is', 1030 + 10)#it keeps printing the comma, Im not sure
+#why. I just added a plugin to allow me to run my py program inside my editor
+print("this is another \rexample")
+''' now to delimit
+all of
+these
+comments
+print("john cena")'''
+print("ron chena")
+# onto variables
+x = 10
+print(x)
+print("x=", x) #it keeps printing the comma, idk why
+x = x + 2
+print(x)
+print(x)
