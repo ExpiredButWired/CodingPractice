c733baaa8f8f9c59ea9a96c233054e9d219a4631
j5E01 <j5E01@users.noreply.github.com>
2019-01-25 17:55:00 +0100

    some notes on the error

--- literallyrestart.py
+++ literallyrestart.py
@@ -1,5 +1,9 @@
 import pygame, sys
 miles_driven = input("enter miles driven\n ")
 gallons_used = input("enter gallons used\n")
+#above, you asked the user to input a string. The miles_driven and gallons_used are still strings "number"
+
+#below, you must convert the strings into integers, or floats (if they contain decimals)
+# int(miles_driven) will turn a "45" into 45. same with gallons_used
 x = miles_driven / gallons_used
 print('your MPG =', x)
