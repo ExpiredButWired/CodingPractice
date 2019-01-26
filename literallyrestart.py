900fdf74f8bd6666b857916c91efb5a60098017a
B_cuz <yodudewuzzup@gmail.com>
2019-01-26 17:12:10 -0500

    Update literallyrestart.py

--- literallyrestart.py
+++ literallyrestart.py
@@ -1,5 +1,12 @@
 import pygame, sys
+#this calculates MPG
+print('this program calculates MPG')
+
 miles_driven = input("enter miles driven\n ")
+miles_driven = float(miles_driven)
 gallons_used = input("enter gallons used\n")
+gallons_used = float(gallons_used)
 x = miles_driven / gallons_used
 print('your MPG =', x)
+#running script runner extension w/ atom editor makes this code read exactly
+#(your MPG =', 50
