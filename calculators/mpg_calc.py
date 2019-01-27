b281a0f041d32daaac08999bc82ece459748a75d
B_cuz <yodudewuzzup@gmail.com>
2019-01-26 22:00:30 -0500

    a few different calcs

---
+++ calculators/mpg_calc.py
@@ -0,0 +1,10 @@
+import pygame, sys
+#this calculates MPG
+print('this program calculates MPG')
+
+miles_driven = float(input("enter miles driven\n "))
+gallons_used = float(input("enter gallons used\n"))
+x = miles_driven / gallons_used
+print('your MPG =', x)
+#running script runner extension w/ atom editor makes this code read exactly
+#(your MPG =', 50
